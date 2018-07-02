a45207e234eff160def25cd8acd5fb6fcb97ed21
benpakmnz <benpakmnz@gmail.com>
2018-07-02 18:03:59 +0300

    Add files via upload

--- js/post_script.js
+++ js/post_script.js
@@ -173,10 +173,46 @@ class Post{
 
         this.removeButton = this.el.querySelectorAll('li')[1];
         this.removeButton.addEventListener('click', () => this.remove());
+        this.likeButton = this.el.querySelector('.react-button.like');
+        this.likeButton.addEventListener('click', () => this.addReaction());
+        this.reactionCounter= 0;
+        this.postEditButton = this.el.querySelector('.post-top .icons.more.post ul li:nth-child(1)');
+        this.postEditButton.addEventListener('click', () => this.editPost());
     }
     remove() {
         this.el.parentNode.removeChild(this.el);
       }
+
+    addReaction() { 
+    this.reaction = this.el.querySelector('.reaction-status');
+    this.reactionStatus = this.reaction.querySelector('span');
+        // if(this.reactionStatus.innerHTML>0){
+        //     this.number = this.reactionStatus.innerHTML;
+        //     console.log (this.number)
+        //     this.reactionCounter + this.number + 1;
+        // }else{
+            this.reactionCounter ++;
+        // }
+    this.reactionStatus.innerText = this.reactionCounter;
+    this.reaction.style.display = "flex";  
+
+    }
+    editPost(){
+        this.textarea2Edit = this.el.querySelector('.post-text');
+        this.text2Edit = this.textarea2Edit.innerText;
+        this.textarea2Edit.innerHTML=`
+        <div class="post-text edit"><textarea rows="4" cols="50">${this.text2Edit}</textarea>
+        <button>Post</button>
+        `
+        this.postEditApproveButton = this.el.querySelector('.post-text .post-text.edit button');
+        this.postEditApproveButton.addEventListener('click', () => this.editApprove());
+    }
+    editApprove(){
+        this.editedText= this.el.querySelector('.post-text .post-text.edit');
+        this.textArea2= this.editedText.querySelector('textarea');
+        this.text2Save = this.textArea2.value;
+        this.textarea2Edit.innerHTML=`<div class="post-text">${this.text2Save}</div>`;
+    }
 }
 
 let mainEl = document.querySelector('.main');
